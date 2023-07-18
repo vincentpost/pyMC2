ed0098d940883de76a1e6686ed9bbfd199d2dd72
vincentpost <info@edinsi.nl>
2023-07-18 16:39:16 +0200

    created first_exercise.py

---
+++ first_exercise.py
@@ -0,0 +1,13 @@
+"""
+This file is a basic Python program that displays a message on the screen.
+The contents of the message vary depending on the name provided.
+"""
+
+name = "YourName"
+
+if name == "Vincent":
+    print("Vincent is one of the instructors of the Python Masterclass")
+elif name == "Anushree":
+    print("Anushree is the AWS support person during the Python Masterclass")
+else:
+    print("Name is unknown, not sure what to print about this person.")
